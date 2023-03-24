4d53e86e41eb2150e0552abef32190901d01dca4
Wes <signerwesley@gmail.com>
2023-03-24 15:22:14 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace TempConv
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double farhenheit;
+            double celsius;
+            string input;
+            Console.WriteLine("Temperature in F:");
+            input = Console.ReadLine();
+            farhenheit = double.Parse(input);
+
+            celsius = (farhenheit - 32) *5 / 9;
+            Console.WriteLine("The Temperature in C is: " + celsius);
+            Console.ReadLine();
+        }
+    }
+}
